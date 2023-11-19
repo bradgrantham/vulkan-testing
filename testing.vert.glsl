d2358ebbfc2a0677b50346ee0b889e2d2de31ce8
Brad Grantham <grantham@plunk.org>
2023-11-19 09:49:57 -0800

    temporarily draw hardcoded triangle

--- testing.vert.glsl
+++ testing.vert.glsl
@@ -1,5 +1,11 @@
 #version 450
 
+vec3 positions[3] = vec3[](
+    vec3(0.0, -0.5, -0.5),
+    vec3(0.5, 0.5, -0.5),
+    vec3(-0.5, 0.5, -0.5)
+);
+
 layout (location = 0) in vec3 input_position;
 layout (location = 1) in vec3 input_color;
 
@@ -12,6 +18,6 @@ out gl_PerVertex
 
 void main() 
 {
-   gl_Position = /* ubo.projectionMatrix * ubo.viewMatrix * ubo.modelMatrix * */ vec4(input_position.xyz, 1.0);
+   gl_Position = vec4(positions[gl_VertexIndex], 1.0); // /* ubo.projectionMatrix * ubo.viewMatrix * ubo.modelMatrix * */ vec4(input_position.xyz, 1.0);
    outColor = vec4(input_color.xyz, 1.0);
 }
