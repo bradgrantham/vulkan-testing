8013680615de3a372c3b4b05c4c69e046d1d2975
Brad Grantham <grantham@plunk.org>
2023-12-06 23:59:21 -0800

    pass light color and position from app

--- testing.frag.glsl
+++ testing.frag.glsl
@@ -1,5 +1,11 @@
 #version 450
 
+layout (binding = 1) uniform UBO2
+{
+    vec3 light_position;
+    vec3 light_color;
+} ubo2;
+
 layout (location = 0) in vec4 vertex_position;
 layout (location = 1) in vec3 vertex_normal;
 layout (location = 2) in vec4 vertex_color;
@@ -14,14 +20,12 @@ void main()
     if(dot(normal, edir) < 0) {
         normal *= -1;
     }
-    vec3 light_pos = vec3(100, 100, 100); // light_position uniform
-    vec3 light_color = vec3(1, 1, 1); // light_color uniform
-    vec3 ldir = normalize(light_pos - vertex_position.xyz);
+    vec3 ldir = normalize(ubo2.light_position - vertex_position.xyz);
     vec3 refl = reflect(-ldir, normal);
-    vec3 diffuse = max(0, dot(normal, ldir)) * light_color; // * texture(material_diffuse_texture, vertex_texcoord);
-    vec3 ambient = light_color;
+    vec3 diffuse = max(0, dot(normal, ldir)) * ubo2.light_color; // * texture(material_diffuse_texture, vertex_texcoord);
+    vec3 ambient = ubo2.light_color;
     // let specular = vec3<f32>(0, 0, 0); 
-    vec3 specular = pow(max(0, dot(refl, edir)), 50 /* material_shininess */) * light_color;
+    vec3 specular = pow(max(0, dot(refl, edir)), 50 /* material_shininess */) * ubo2.light_color;
     vec3 material_diffuse = vec3(.8, .8, .8);
     vec3 material_ambient = vec3(.1, .1, .1);
     vec3 material_specular = vec3(.8, .8, .8);
